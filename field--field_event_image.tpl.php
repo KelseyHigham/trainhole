d6aa83160717cb2a7d676d130b68e2e8c3e28b59
Kelsey Higham <kelsey.higham@gmail.com>
2012-10-03 01:31:35 -0700

    for easier comparison to field--field_game_screenshot.tpl.php
    (with QuickLook)

--- field--field_event_image.tpl.php
+++ field--field_event_image.tpl.php
@@ -1,6 +1,4 @@
-<?php /* This code is stolen from the game screenshot code; so a bunch of it doesn't make sense. For instance, events are never shown in teaser form, and they use images, not screenshots. */
-
-if ($element['#view_mode'] == 'full'): // full page, or teaser (front page) ?>
+<?php if ($element['#view_mode'] == 'full'): // full page, or teaser (front page) ?>
 	<?php foreach ($items as $delta => $item) : // print all the screenshots ?>
 		<div class="screenshot" style="width: 940px; float: left; text-align: center; ">
 			<?php print render($item); ?>
@@ -8,4 +6,8 @@ if ($element['#view_mode'] == 'full'): // full page, or teaser (front page) ?>
 	<?php endforeach; ?>
 <?php else: ?>
 	<?php print render($items[0]) // print just the first one ?>
-<?php endif; ?>
+<?php endif; 
+
+/* This code is stolen from the game screenshot code; so a bunch of it doesn't make sense. For instance, events are never shown in teaser form, and they use images, not screenshots. */
+
+?>
