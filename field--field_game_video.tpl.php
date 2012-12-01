c2eb21a1a536ed33c11035c1307c405b6bae199c
Kelsey Higham <kelsey.higham@gmail.com>
2012-11-30 17:45:48 -0800

    Removed an unnecessary 20px lower margin.

--- field--field_game_video.tpl.php
+++ field--field_game_video.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach ($items as $delta => $item) : ?>
 	<?php if (getVimeoId(render($item))) : ?>
-		<iframe width="700" height="394" style="float: left;" src="http://player.vimeo.com/video/<?= getVimeoId(render($item)) ?>?portrait=0&amp;color=ffffff" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+		<iframe width="700" height="394" style="float: left; margin-bottom: 0;" src="http://player.vimeo.com/video/<?= getVimeoId(render($item)) ?>?portrait=0&amp;color=ffffff" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 	<?php else : ?>
-		<iframe width="700" height="424" style="float: left;" src="http://www.youtube.com/embed/<?= getYoutubeId(render($item)) ?>?rel=0" frameborder="0" allowfullscreen></iframe>
+		<iframe width="700" height="424" style="float: left; margin-bottom: 0;" src="http://www.youtube.com/embed/<?= getYoutubeId(render($item)) ?>?rel=0" frameborder="0" allowfullscreen></iframe>
 	<?php endif; ?>
 <?php endforeach;
